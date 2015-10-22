6e755fed0420fd686955529253ea957f15ffc542
xera518 <xera518@users.noreply.github.com>
2015-10-22 12:39:29 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<script type"text/javascript">
+			function initialize(){
+				imgElem = document.getElementById("image");
+				width = imgElem.naturalWidth;
+				height = imgElem.naturalHeight;
+			
+				imgElem.width = width*2;
+				imgElem.height = height*2;
+			}
+			
+			function generateImage(){
+				imgElem = document.getElementById("image");
+				R = document.getElementById("R").value;
+				G = document.getElementById("G").value;
+				B = document.getElementById("B").value;
+				mode = (document.getElementById("mode").checked ? 1 : 0);
+				imageID = document.getElementById("imageID").options[document.getElementById("imageID").selectedIndex].innerHTML;
+				img = "convert.php?img=" + imageID + "&mode=" + mode + "&r=" + R + "&g=" + G +"&b=" + B;
+					
+				imgElem.setAttribute("src", img);
+				
+				imgElem.onload = function(){
+					initialize();
+				}
+				
+			}
+		</script>
+	</head>
+	<body onload="initialize()">
+	<?php
+		echo '<form>';
+		echo	'<select id="imageID" size="1">';
+		$files = glob('img/*.{png}', GLOB_BRACE);
+		$value = 0;
+		foreach($files as $file) {	
+			 echo '<option value='.$value.'>'.$file.'</option>';
+			 $value++;
+		}
+		echo '</select>';
+		echo '</form>';
+	?>
+		<form>
+			<p>R: <input type="number" min="0" value="150" max="255" id="R"></p>
+			<p>G: <input type="number" min="0" value="0" max="255" id="G"></p>
+			<p>B: <input type="number" min="0" value="250" max="255" id="B"></p>
+			<p><input type="checkbox" id="mode"> Alternative mode?</p>
+			<p><input class="button" type="button" value="Generate" onClick="JavaScript:generateImage()"></p>
+		</form>
+		<img id="image" src="convert.php?img=img/0.png">
+	</body>
+</html>
