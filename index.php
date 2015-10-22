23deeebbc1d45f3d496d1176955d58a43cbf3de7
xera518 <xera518@users.noreply.github.com>
2015-10-22 13:00:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -30,25 +30,25 @@
 		</script>
 	</head>
 	<body onload="initialize()">
-	<?php
-		echo '<form>';
-		echo	'<select id="imageID" size="1">';
-		$files = glob('img/*.{png}', GLOB_BRACE);
-		$value = 0;
-		foreach($files as $file) {	
-			 echo '<option value='.$value.'>'.$file.'</option>';
-			 $value++;
-		}
-		echo '</select>';
-		echo '</form>';
-	?>
-		<form>
-			<p>R: <input type="number" min="0" value="150" max="255" id="R"></p>
-			<p>G: <input type="number" min="0" value="0" max="255" id="G"></p>
-			<p>B: <input type="number" min="0" value="250" max="255" id="B"></p>
-			<p><input type="checkbox" id="mode"> Alternative mode?</p>
-			<p><input class="button" type="button" value="Generate" onClick="JavaScript:generateImage()"></p>
-		</form>
-		<img id="image" src="convert.php?img=img/0.png">
+		<div class="content">
+			<form>
+			<?php
+				echo	'<select id="imageID" size="1">';
+				$files = glob('img/*.{png}', GLOB_BRACE);
+				$value = 0;
+				foreach($files as $file) {	
+					 echo '<option value='.$value.'>'.$file.'</option>';
+					 $value++;
+				}
+				echo '</select>';
+			?>
+				<p>R: <input type="number" min="0" value="150" max="255" id="R"></p>
+				<p>G: <input type="number" min="0" value="0" max="255" id="G"></p>
+				<p>B: <input type="number" min="0" value="250" max="255" id="B"></p>
+				<p><input type="checkbox" id="mode"> Alternative mode?</p>
+				<p><input class="button" type="button" value="Generate" onClick="JavaScript:generateImage()"></p>
+			</form>
+			<img id="image" src="convert.php?img=img/0.png">
+		</div>
 	</body>
 </html>
