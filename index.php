63374f94b50739e089cc86116c75851db4fb0790
xera518 <xera518@users.noreply.github.com>
2015-10-31 21:23:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,30 +25,34 @@
 				imgElem.onload = function(){
 					initialize();
 				}
-				
 			}
 		</script>
 	</head>
 	<body onload="initialize()">
 		<div class="content">
-			<form>
-			<?php
-				echo	'<select id="imageID" size="1">';
-				$files = glob('img/*.{png}', GLOB_BRACE);
-				$value = 0;
-				foreach($files as $file) {	
-					 echo '<option value='.$value.'>'.$file.'</option>';
-					 $value++;
-				}
-				echo '</select>';
-			?>
-				<p>R: <input type="number" min="0" value="150" max="255" id="R"></p>
-				<p>G: <input type="number" min="0" value="0" max="255" id="G"></p>
-				<p>B: <input type="number" min="0" value="250" max="255" id="B"></p>
-				<p><input type="checkbox" id="mode"> Alternative mode?</p>
-				<p><input class="button" type="button" value="Generate" onClick="JavaScript:generateImage()"></p>
-			</form>
-			<img id="image" src="convert.php?img=img/0.png">
+			<div class="input">
+				<form>
+				<?php
+					echo '<select id="imageID" size="1">'.PHP_EOL;
+					$files = glob('img/*.{png}', GLOB_BRACE);	//all .png files from the img/ folder are being dropped in a dropdown menu
+					$value = 0;
+					foreach($files as $file) {	
+						 echo '<option value='.$value.'>'.str_replace('img/','',$file).'</option>'.PHP_EOL;
+						 $value++;
+					}
+					echo '</select>'.PHP_EOL;
+				?>
+					<p>R: <input type="number" min="0" value="150" max="255" id="R"></p>
+					<p>G: <input type="number" min="0" value="0" max="255" id="G"></p>
+					<p>B: <input type="number" min="0" value="250" max="255" id="B"></p>
+					<p><input type="checkbox" id="mode" text="test"> <label for="mode">Alternative mode</label></p>
+					<p>Some sprites may using a mask and are only partially colored. (ex. 106, 117, 45, 48 etc.)</p>
+					<p><input class="button" type="button" value="Generate" onClick="JavaScript:generateImage()"></p>
+				</form>
+			</div>
+			<div class="output">
+				<img id="image" src="convert.php?img=0.png">
+			</div>
 		</div>
 	</body>
 </html>
